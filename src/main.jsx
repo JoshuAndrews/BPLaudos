262c649a12f4a918e0f8e613f9625ec2a0c677ba
JoshuAndrews <108185859+JoshuAndrews@users.noreply.github.com>
2025-10-31 15:06:33 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Laudos from './laudos.jsx'; 
+
+import './laudos.scss'; 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Laudos />
+  </React.StrictMode>,
+);
